2061fc1c480496ca6a17e705c92e2ef337b6c5df
BoVlad <minecraft@karnyz.com>
2025-12-15 19:02:36 +0200

    1

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-
 import email_validator
 
 
